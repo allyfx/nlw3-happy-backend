f116f929bbef3c4542d968d3192d05fbb79ada09
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-13 16:30:13 -0300

    Handle exceptions

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,18 @@
 import express from 'express';
 import path from 'path';
+import 'express-async-errors';
+
 import './database/connection';
 
 import routes from './routes';
+import errorHanlder from './errors/handler';
 
 const app = express();
 
 app.use(express.json());
 app.use(routes);
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorHanlder);
 
 app.listen(3333, () => {
     console.log('Server started!');
