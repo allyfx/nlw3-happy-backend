35aac954db4052ad413c5f36ea3bc517003946bb
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-13 14:57:32 -0300

    Create database connection and create orphanages table in database

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 
+import './database/connection';
+
 const app = express();
+
+app.use(express.json());
 
 app.listen(3333, () => {
     console.log('Server started!');
