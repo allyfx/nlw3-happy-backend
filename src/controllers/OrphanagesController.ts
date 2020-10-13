9eddf5a380fef2084a469bd4094b1d8fc6118afc
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-13 16:07:24 -0300

    Images Upload added

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -3,6 +3,24 @@ import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 
 export default {
+    async index(request: Request, response: Response) {
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanages = await orphanagesRepository.find();
+
+        return response.json(orphanages);
+    },
+
+    async show(request: Request, response: Response) {
+        const { id } = request.params;
+
+        const orphanagesRepository = getRepository(Orphanage);
+
+        const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+        return response.json(orphanage);
+    },
+
     async create(request: Request, response: Response) {
         const {
             name,
@@ -15,6 +33,11 @@ export default {
         } = request.body;
     
         const orphanagesRepository = getRepository(Orphanage);
+
+        const requestImages = request.files as Express.Multer.File[];
+        const images = requestImages.map(image => {
+            return { path: image.filename }
+        });
     
         const orphanage = orphanagesRepository.create({
             name,
@@ -23,7 +46,8 @@ export default {
             about,
             instructions,
             opening_hours,
-            open_on_weekends
+            open_on_weekends,
+            images
         });
     
         await orphanagesRepository.save(orphanage);
