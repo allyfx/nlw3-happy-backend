53f423150b2b68f97a40821b5f35137ea19bf2f2
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-13 16:24:21 -0300

    View added

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -1,14 +1,18 @@
 import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
+import orphanage_view from '../views/orphanages_view';
+
 import Orphanage from '../models/Orphanage';
 
 export default {
     async index(request: Request, response: Response) {
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        });
 
-        return response.json(orphanages);
+        return response.json(orphanage_view.renderMany(orphanages));
     },
 
     async show(request: Request, response: Response) {
@@ -16,9 +20,11 @@ export default {
 
         const orphanagesRepository = getRepository(Orphanage);
 
-        const orphanage = await orphanagesRepository.findOneOrFail(id);
+        const orphanage = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return response.json(orphanage);
+        return response.json(orphanage_view.render(orphanage));
     },
 
     async create(request: Request, response: Response) {
@@ -52,6 +58,6 @@ export default {
     
         await orphanagesRepository.save(orphanage);
     
-        return response.status(201).json(orphanage);
+        return response.status(201).json(orphanage_view.render(orphanage));
     }
 }
