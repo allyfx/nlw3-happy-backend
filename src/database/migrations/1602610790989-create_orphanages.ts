872035cbd0caefc14511b71bb7b34f908a4e9a06
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-13 15:29:03 -0300

    Create orphanage route added

--- src/database/migrations/1602610790989-create_orphanages.ts
+++ src/database/migrations/1602610790989-create_orphanages.ts
@@ -39,6 +39,10 @@ export default class createOrphanages1602610790989 implements MigrationInterface
                     type: 'text',
                 },
                 {
+                    name: 'opening_hours',
+                    type: 'varchar',
+                },
+                {
                     name: 'open_on_weekends',
                     type: 'boolean',
                     default: false,
