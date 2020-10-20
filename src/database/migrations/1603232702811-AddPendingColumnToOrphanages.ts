123683b7d4f94795a3671b504a5d6563952e292a
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2020-10-20 19:29:23 -0300

    2.0 | Create Migration AddPendingColumnToORphanages

---
+++ src/database/migrations/1603232702811-AddPendingColumnToOrphanages.ts
@@ -0,0 +1,17 @@
+import {MigrationInterface, QueryRunner, TableColumn} from "typeorm";
+
+export default class AddPendingColumnToOrphanages1603232702811 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.addColumn('orphanages', new TableColumn({
+            name: 'pending',
+            type: "boolean",
+            default: false,
+        }))
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.dropColumn('orphanages', 'pending');
+    }
+
+}
